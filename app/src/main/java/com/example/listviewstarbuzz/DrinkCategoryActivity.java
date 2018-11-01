88e0a8b815be294a378f9e645891f2a17e1c1f98
JennaN23 <jennan@outlook.com>
2018-11-01 09:03:27 -0700

    Completed the DrinkActivity and DrinkCategoryActivity

--- app/src/main/java/com/example/listviewstarbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/listviewstarbuzz/DrinkCategoryActivity.java
@@ -2,6 +2,15 @@ package com.example.listviewstarbuzz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+import android.widget.AdapterView;
+import android.app.Activity;
+
+import com.hfad.starbuzz.Drink;
+import com.hfad.starbuzz.DrinkActivity;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
 
@@ -9,5 +18,26 @@ public class DrinkCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> listDrinks,
+                                             View itemView,
+                                             int position,
+                                             long id) {
+                        Intent intent = new Intent(DrinkCategoryActivity.this,
+                            DrinkActivity.class);
+                    intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                    startActivity(intent);
+                    }
+                };
+
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
